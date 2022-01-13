b131f617eadeb2fd798966b5f6b4f18463173eab
brenobbfreitas <brenobbfreitas@gmail.com>
2022-01-12 23:00:38 -0300

    comentarios C#

---
+++ CSharp/CSharp/Fundamentos/Comentarios.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Fundamentos
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    internal class Comentarios
+    {
+        public static void Executar(int a) {
+            // Cuidado com os comentarios desnecessários
+            Console.WriteLine("Código claro é sempre melhor");
+            /*
+             * Esse é um comentário 
+             * de múltiplas linhas...
+             */
+            Console.WriteLine("O C# tem o XML Comments")
+        }
+    }
+}
---
+++ CSharp/CSharp/Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Fundamentos
+{
+    internal class PrimeiroPrograma
+    {
+       public static void Executar() {
+            Console.Write("Primeiro ");
+            Console.WriteLine("Progama");
+            Console.WriteLine("Terminou!");
+        }
+    }
+}
