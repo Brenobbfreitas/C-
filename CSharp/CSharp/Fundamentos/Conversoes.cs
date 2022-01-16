44f1ac6f1482a3717835bdd8d3f375c130e72a61
brenobbfreitas <brenobbfreitas@gmail.com>
2022-01-16 14:12:54 -0300

    Conversoes

--- CSharp/CSharp/Fundamentos/Conversoes.cs
+++ CSharp/CSharp/Fundamentos/Conversoes.cs
@@ -18,6 +18,25 @@ namespace CSharp.Fundamentos
             double nota = 9.6;
             int notaTruncada = (int) nota;
             Console.WriteLine("Nota truncada: {0}", notaTruncada);
+
+            Console.WriteLine("Digite sua idade: ");
+            string idadeString = Console.ReadLine();
+            int idadeInteiro = int.Parse(idadeString);
+            Console.WriteLine("Sua idade é de {0}",idadeInteiro);
+
+            idadeInteiro = Convert.ToInt32(idadeString);
+            Console.WriteLine("Resultado: {0}", idadeInteiro);
+
+
+            Console.WriteLine("Digite um valor: ");
+            string palavra = Console.ReadLine();
+            int numero1;
+            int.TryParse(palavra, out numero1); //metodo mais segura 
+            Console.WriteLine("Resultado 1: {0}",numero1);
+
+            Console.WriteLine("Digite o segundo valor: ");
+            int.TryParse(Console.ReadLine(), out int numero2); //metodo otimizado
+            Console.WriteLine("Resultado 2: {0}", numero2);
         }
     }
 }
