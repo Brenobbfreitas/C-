fad21e122efc1ec781caee26103da6aee06e9590
brenobbfreitas <brenobbfreitas@gmail.com>
2022-01-19 15:09:05 -0300

    get/set

--- CSharp/CSharp/ClassesEMetodos/GetSet.cs
+++ CSharp/CSharp/ClassesEMetodos/GetSet.cs
@@ -11,12 +11,67 @@ namespace CSharp.ClassesEMetodos
     {
         private string Marca;
         private string Modelo; //Quando o valor é privado vc precisa de um metodo para acessalo
-        private int Cilindrada; //sempre um valor positivo/ SETA o valor do atributo quando quer alter/ ou ler o valor GET
+        private uint Cilindrada; //sempre um valor positivo/ SETA o valor do atributo quando quer alter/ ou ler o valor GET
+        //uint valor obrigatoriamente positivo
+        public Moto(string marca, string modelo, uint cilindrada) { // Construtor
+            //Marca = marca;
+            //Modelo = modelo;
+            //Cilindrada = cilindrada;
+            SetMarca(marca);
+            SetModelo(modelo);
+            SetCilindrada(cilindrada);
+
+        }
+
+        public Moto() { //construtor
+        
+        }
+
+        public string GetMarca() { // operação de leitura
+            return Marca;
+        }
+
+        public void SetMarca(string marca) { // alterar a marca
+            Marca = marca;
+        }
+
+        public string GetModelo() {
+            return Modelo;
+        }
+
+        public void SetModelo(string modelo) {
+            Modelo = modelo;
+        }
+
+        public uint GetCilindrada() {
+            return Cilindrada;
+        }
+    
+        public void SetCilindrada(uint cilindrada) {
+            // 1 Opção
+            //if(cilindrada > 0) {
+            //Cilindrada = cilindrada;
+            //}
+            // 2 Opção
+            /*Cilindrada = Math.Abs(cilindrada);*/ //transforma valores negativos em positivos
+            Cilindrada = cilindrada;
+        }
     }
     internal class GetSet
     {
         public static void Executar() {
+            var moto1 = new Moto("kawasaki", "Ninja ZX-GR", 636);
 
+            Console.WriteLine(moto1.GetMarca());
+            Console.WriteLine(moto1.GetModelo());
+            Console.WriteLine(moto1.GetCilindrada());
+
+            var moto2 = new Moto();
+            moto2.SetModelo("CG Titan");
+            moto2.SetMarca("Honda");
+            moto2.SetCilindrada(150);
+
+            Console.WriteLine(moto2.GetMarca()+" "+moto2.GetModelo()+" "+moto2.GetCilindrada() +);
         }
     }
 }
